e9e04d638c4801e1ff4533a34207d7b4ea018b9e
Chulwoo Park <park@chulwoo.dev>
2020-12-19 21:26:00 +0900

    chore: initial commit

---
+++ app/src/main/java/dev/chulwoo/hp/test/MainActivity.kt
@@ -0,0 +1,18 @@
+package dev.chulwoo.hp.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.chulwoo.hp.test.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/dev/chulwoo/hp/test/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package dev.chulwoo.hp.test.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+}
