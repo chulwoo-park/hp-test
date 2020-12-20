a2f227940b2b6d5b25cc8d557476b1a5ba7a4ff0
Chulwoo Park <park@chulwoo.dev>
2020-12-20 14:24:41 +0900

    feat(user): implement UserListViewModel

--- app/src/main/java/dev/chulwoo/hp/test/feature/user/presentation/viewmodel/UserListViewModel.kt
+++ app/src/main/java/dev/chulwoo/hp/test/feature/user/presentation/viewmodel/UserListViewModel.kt
@@ -1,24 +1,45 @@
 package dev.chulwoo.hp.test.feature.user.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dev.chulwoo.hp.test.feature.user.domain.usecase.FilterUsers
+import dev.chulwoo.hp.test.feature.user.domain.usecase.FilterUsersParam
 import dev.chulwoo.hp.test.feature.user.domain.usecase.GetSortedUsers
 import dev.chulwoo.hp.test.feature.user.presentation.model.UserListState
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class UserListViewModel(
     private val getSortedUsers: GetSortedUsers,
-    private val filterUsers: FilterUsers
+    private val filterUsers: FilterUsers,
+    private val dispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
-    val states: StateFlow<UserListState> = MutableStateFlow(UserListState.Initial)
+    private val _states: MutableStateFlow<UserListState> = MutableStateFlow(UserListState.Initial)
+    val states: StateFlow<UserListState> = _states
 
     fun load() {
-        TODO()
+        if (_states.value is UserListState.Loading) return
+
+        viewModelScope.launch(dispatcher) {
+            _states.value = UserListState.Loading
+            _states.value = try {
+                UserListState.Success(getSortedUsers())
+            } catch (e: Exception) {
+                UserListState.Failure(e)
+            }
+        }
     }
 
     fun filter(keyword: String) {
-        TODO()
+        viewModelScope.launch(dispatcher) {
+            try {
+                val users = filterUsers(FilterUsersParam(keyword))
+                _states.value = UserListState.Success(users)
+            } catch (e: Exception) {
+            }
+        }
     }
 }
