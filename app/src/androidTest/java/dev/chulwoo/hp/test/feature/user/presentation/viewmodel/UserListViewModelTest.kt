5f58d2baa6418cdd6df034988debefe7fca81e03
Chulwoo Park <park@chulwoo.dev>
2020-12-20 14:25:03 +0900

    test(user): fix UserListViewModel test to green

--- app/src/androidTest/java/dev/chulwoo/hp/test/feature/user/presentation/viewmodel/UserListViewModelTest.kt
+++ app/src/androidTest/java/dev/chulwoo/hp/test/feature/user/presentation/viewmodel/UserListViewModelTest.kt
@@ -1,60 +1,66 @@
 package dev.chulwoo.hp.test.feature.user.presentation.viewmodel
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import androidx.lifecycle.asLiveData
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
 import dev.chulwoo.hp.test.feature.user.domain.model.User
+import dev.chulwoo.hp.test.feature.user.domain.repository.UserRepository
 import dev.chulwoo.hp.test.feature.user.domain.usecase.FilterUsers
-import dev.chulwoo.hp.test.feature.user.domain.usecase.FilterUsersParam
 import dev.chulwoo.hp.test.feature.user.domain.usecase.GetSortedUsers
 import dev.chulwoo.hp.test.feature.user.presentation.model.UserListState
-import kotlinx.coroutines.flow.drop
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.take
-import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.runBlocking
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.equalTo
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class UserListViewModelTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    val dispatcher = TestCoroutineDispatcher()
+
     private lateinit var getSortedUsers: GetSortedUsers
     private lateinit var filterUsers: FilterUsers
     private lateinit var viewModel: UserListViewModel
+    private lateinit var observer: Observer<UserListState>
 
     @Before
     fun setUp() {
-        getSortedUsers = mock {
-            onBlocking { invoke() } doReturn listOf(User("a"), User("b"))
+        val repository = mock<UserRepository> {
+            onBlocking { getSortedUsers() } doReturn listOf(User("a"), User("b"))
         }
-        filterUsers = mock {
-            onBlocking { invoke(FilterUsersParam("a")) } doReturn listOf(User("a"))
-            onBlocking { invoke(FilterUsersParam("b")) } doReturn listOf(User("b"))
-        }
-        viewModel = UserListViewModel(getSortedUsers, filterUsers)
+        observer = mock {}
+        getSortedUsers = GetSortedUsers(repository)
+        filterUsers = FilterUsers(repository)
+        viewModel = UserListViewModel(getSortedUsers, filterUsers, dispatcher)
+        viewModel.states.asLiveData().observeForever(observer)
     }
 
     @Test
     fun testLoad() {
-        runBlocking {
+        dispatcher.runBlockingTest {
+            verify(observer).onChanged(UserListState.Initial)
             viewModel.load()
-            val states = viewModel.states.take(3).toList()
-            assertThat(states[0], equalTo(UserListState.Initial))
-            assertThat(states[1], equalTo(UserListState.Loading))
-            assertThat(states[2], equalTo(UserListState.Success(listOf(User("a"), User("b")))))
+            verify(observer).onChanged(UserListState.Loading)
+            verify(observer).onChanged(UserListState.Success(listOf(User("a"), User("b"))))
         }
     }
 
     @Test
     fun testFilter() {
-        runBlocking {
+        dispatcher.runBlockingTest {
             viewModel.filter("a")
-            var state = viewModel.states.take(1).first()
-            assertThat(state, equalTo(UserListState.Success(listOf(User("a")))))
-
             viewModel.filter("b")
-            state = viewModel.states.drop(1).take(1).first()
-            assertThat(state, equalTo(UserListState.Success(listOf(User("b")))))
+
+            verify(observer).onChanged(UserListState.Success(listOf(User("a"))))
+            verify(observer).onChanged(UserListState.Success(listOf(User("b"))))
         }
     }
 }
