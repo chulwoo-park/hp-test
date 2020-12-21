9d6dd1d83fe998fd3f71805c715f5dac35b9cbf0
Chulwoo Park <park@chulwoo.dev>
2020-12-21 10:54:45 +0900

    feat(user): implement simple throttle

--- app/src/main/java/dev/chulwoo/hp/test/feature/user/presentation/viewmodel/UserListViewModel.kt
+++ app/src/main/java/dev/chulwoo/hp/test/feature/user/presentation/viewmodel/UserListViewModel.kt
@@ -7,6 +7,7 @@ import dev.chulwoo.hp.test.feature.user.domain.usecase.FilterUsersParam
 import dev.chulwoo.hp.test.feature.user.domain.usecase.GetSortedUsers
 import dev.chulwoo.hp.test.feature.user.presentation.model.UserListState
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -16,6 +17,8 @@ class UserListViewModel(
     private val filterUsers: FilterUsers,
     private val dispatcher: CoroutineDispatcher
 ) : ViewModel() {
+
+    private var filterJob: Job? = null
 
     private val _states: MutableStateFlow<UserListState> = MutableStateFlow(UserListState.Initial)
     val states: StateFlow<UserListState> = _states
@@ -34,7 +37,8 @@ class UserListViewModel(
     }
 
     fun filter(keyword: String) {
-        viewModelScope.launch(dispatcher) {
+        filterJob?.cancel()
+        filterJob = viewModelScope.launch(dispatcher) {
             try {
                 val users = filterUsers(FilterUsersParam(keyword))
                 _states.value = UserListState.Success(users)
